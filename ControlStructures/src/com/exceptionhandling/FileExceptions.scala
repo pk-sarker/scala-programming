fc2c7d7113d0df6c088b803abcf368fc12b5aca1
Pijus Kumar Sarker <pijus.sarker@gmail.com>
2020-05-05 17:45:48 +0600

    Updated file exception code

--- ControlStructures/src/com/exceptionhandling/FileExceptions.scala
+++ ControlStructures/src/com/exceptionhandling/FileExceptions.scala
@@ -7,20 +7,28 @@ import scala.io.Source
 object FileExceptions extends App {
   var text: List[String] = Nil
   try {
-    val absolute_path_to_file = "/Users/..../scala-programming/ControlStructures/src/com/exceptionhandling"
+    val absolute_path_to_file = "/Users/pijussarker/myws/RND/scala/scala-programming/ControlStructures/src/com/exceptionhandling"
     text = openAndReadAFile(  absolute_path_to_file + "/test.log")
     for ((e, index) <- text.zipWithIndex) {
       println(index + ": " + e)
     }
-
+  } catch {
+    case e: FileNotFoundException => println("Couldn't find that file.")
+    case e: IOException => println("Had an IOException trying to read that file")
+  } finally {
+    println("-- Complete --")
+  }
+  // This code block will throw exception as the file path is not correct
+  try {
     text = openAndReadAFile(   "/wrong_path/test.log")
     for ((e, index) <- text.zipWithIndex) {
       println(index + ": " + e)
     }
-
   } catch {
     case e: FileNotFoundException => println("Couldn't find that file.")
     case e: IOException => println("Had an IOException trying to read that file")
+  } finally {
+    println("-- Complete --")
   }
 
   def openAndReadAFile(filepath: String): List[String] = {
